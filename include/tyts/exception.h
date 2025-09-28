9f1271bf80083aced67104b251cc5d5d113c66ed
shaojx <13315567369@163.com>
2025-09-28 09:31:45 +0800

    feat: add exception class

---
+++ include/tyts/exception.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <cstring>
+#include <exception>
+#include <memory>
+#include <string>
+
+namespace tyts {
+
+class Exception : public std::exception
+{
+public:
+    Exception(const std::string &&fmt)
+    {
+        int length = fmt.length() + 1;
+        errmsg_    = std::shared_ptr<char>(new char[length], std::default_delete<char[]>());
+        memcpy(errmsg_.get(), fmt.c_str(), fmt.length());
+    }
+
+    template <typename... Args>
+    explicit Exception(const std::string &&fmt, Args &&...args)
+    {
+        int length = snprintf(nullptr, 0, fmt.c_str(), args...) + 1;
+        errmsg_    = std::shared_ptr<char>(new char[length], std::default_delete<char[]>());
+        snprintf(errmsg_.get(), length, fmt.c_str(), std::forward<Args>(args)...);
+    }
+
+    virtual char const *what() const noexcept { return errmsg_.get(); }
+
+private:
+    std::shared_ptr<char> errmsg_;
+};
+
+} // namespace tyts
